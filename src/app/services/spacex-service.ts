93c97812912522643c440e5b3a2b721d39b828f7
tanupreetkaur <tanubal21@gmail.com>
2026-04-08 11:19:27 -0400

    Added logic in service for different endpoints

--- src/app/services/spacex-service.ts
+++ src/app/services/spacex-service.ts
@@ -1,6 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Mission } from '../../models/mission.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class SpacexService {}
+
+export class SpacexService {
+  private baseUrl = 'https://api.spacexdata.com/v3/launches';
+
+  constructor(private http: HttpClient){}
+
+  /**
+   * Gets list of all launches/missions
+   */
+  getAllMissions(): Observable<Mission[]> {
+    return this.http.get<Mission[]>(this.baseUrl);
+  }
+
+  /**
+   * Get missions by year
+   */
+  getMissionsByYear(year: String): Observable<Mission[]> {
+    return this.http.get<Mission[]>(`${this.baseUrl}?launch_year=${year}`);
+  }
+
+  /**
+   * Get details for mission by id
+   */
+  getMissionDetailsById(id: number): Observable<Mission> {
+    return this.http.get<Mission>(`${this.baseUrl}/${id}`);
+  }
+} 
