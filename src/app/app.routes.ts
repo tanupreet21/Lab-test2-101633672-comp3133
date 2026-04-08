036bd458bccc39ca11af4e47d6995b5bf5fb5747
tanupreetkaur <tanubal21@gmail.com>
2026-04-08 11:21:32 -0400

    Added List component & details components in routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MissionList } from './components/mission-list/mission-list';
+import { MissionDetails } from './components/mission-details/mission-details';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: MissionList},
+    { path: '/mission/:id', component: MissionDetails}
+];
