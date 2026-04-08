ae9030f28187f16e617ce2cdcd16baddc531e13c
tanupreetkaur <tanubal21@gmail.com>
2026-04-08 13:32:45 -0400

    Added logic for details component & fixed some styling

--- src/app/components/mission-details/mission-details.ts
+++ src/app/components/mission-details/mission-details.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { Mission } from '../../../models/mission.model';
+import { SpacexService } from '../../services/spacex-service';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-mission-details',
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatCardModule,
+    MatButtonModule
+  ],
   templateUrl: './mission-details.html',
   styleUrl: './mission-details.css',
 })
-export class MissionDetails {}
+export class MissionDetails {
+  mission?: Mission;
+
+  constructor(
+    private route: ActivatedRoute,
+    private spacexService: SpacexService
+  ){}
+
+  ngOnInit(){
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.spacexService.getMissionDetailsById(id).subscribe({
+      next: (data) => {
+        this.mission = data;
+      },
+      error: (err) => {
+        console.error('Error fetching mission details: ', err);
+      }
+    });
+  }
+}
